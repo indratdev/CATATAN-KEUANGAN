a58d3ee043591728a09cf1e7dbfc6e7fc14be8fb
indratdev <indrat.dev@gmail.com>
2021-02-25 22:09:37 +0700

    Initial Commit

---
+++ CATATAN KEUANGAN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CATATAN KEUANGAN
+//
+//  Created by IndratS on 25/02/21.
+//  Copyright © 2021 IndratSaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
